02fa5afee7567c2ffcce8c590b97ab276f0705ae
Unwoo <73688472+unwoo52@users.noreply.github.com>
2023-03-24 23:16:35 +0900

    add animation
    라이플맨, 빈손 캐릭터 애니메이션 블렌드 트리  추가

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,19 +1,20 @@
 using Photon.Pun;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviourPun
 {
     [SerializeField] private VariableJoystick variableJoystick;
     [SerializeField] private AnimContoller animContoller;
-    [SerializeField] private float RotateSpeed;
+    [SerializeField] private float rotateSpeed;
+    private Camera _camera;
 
 
     private void Start()
     {
         variableJoystick = CanvasManagement.Instance.VariableJoystick.GetComponent<VariableJoystick>();
         animContoller = GetComponent<AnimContoller>();
+
+        _camera = Camera.main;
     }
 
     public void FixedUpdate()
@@ -22,17 +23,32 @@ public class PlayerMovement : MonoBehaviourPun
         {
             return;
         }
-
-
-
+        /*
         Vector3 target = new Vector3(variableJoystick.Horizontal, 0, variableJoystick.Vertical);
 
+        Quaternion cameraRotation = _camera.transform.rotation;
+        float angle = Quaternion.Angle(Quaternion.identity, cameraRotation);
 
-        if (variableJoystick.Horizontal == 0 && variableJoystick.Vertical == 0) { animContoller.OnStopMove(); }
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.up);
+        Vector3 rotatedVector = rotation * target;
+        */
+
+
+        if (variableJoystick.Horizontal == 0 && variableJoystick.Vertical == 0)
+        {
+            animContoller.OnSetHorizontalParam(0);
+            animContoller.OnSetVerticalParam(0);
+        }
         else
         {
-            animContoller.OnSetVerticalParam(variableJoystick.Vertical);
+            //animContoller.OnLookat(transform.position + rotatedVector, RotateSpeed);
+            //animContoller.OnOnlyMove();
             animContoller.OnSetHorizontalParam(variableJoystick.Horizontal);
+            animContoller.OnSetVerticalParam(variableJoystick.Vertical);
+            if (Mathf.Abs(variableJoystick.Horizontal) > 0.3f)
+            {
+                transform.Rotate(Vector3.up, variableJoystick.Horizontal * rotateSpeed * Time.deltaTime);
+            }
         }
     }
 }
